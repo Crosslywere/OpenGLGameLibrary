3955853db4176bfb0d726752b8b87401c0fc9d00
Crosslywere <ogboruj@outlook.com>
2024-12-26 10:47:45 +0100

    Added glfw to premake

--- premake5.lua
+++ premake5.lua
@@ -2,6 +2,25 @@ workspace "OpenGLGameLibrary"
 	configurations { "Debug", "Release" }
 	platforms "x64"
 
+	startproject "GameLibrary"
+
+	project "glfw"
+		location "vendors/glfw"
+		kind "StaticLib"
+		language "C"
+		cdialect "C17"
+		targetdir "bin/%{prj.name}"
+		objdir "obj/%{prj.name}"
+		files {
+			"vendors/glfw/src/**.c",
+			"vendors/glfw/include/**.h"
+		}
+		vpaths {
+			["Header"] = "**.h",
+			["Source"] = "**.c"
+		}
+		defines { "_GLFW_WIN32", "_CRT_SECURE_NO_WARNINGS" }
+
 	project "GameLibrary"
 		location "%{prj.name}"
 		kind "ConsoleApp"
@@ -21,8 +40,10 @@ workspace "OpenGLGameLibrary"
 		includedirs {
 			"%{prj.name}/include",
 			"vendors/glad/include",
+			"vendors/glfw/include",
 			"vendors/glm"
 		}
+		links "glfw"
 
 		filter "configurations:Debug"
 			defines "DEBUG"
