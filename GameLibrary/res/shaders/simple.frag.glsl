e6baae8a2878ca8ac921cef53c1cf1de6a71bea8
Crosslywere <ogboruj@outlook.com>
2024-12-28 20:27:58 +0100

    Modified shaders

--- GameLibrary/res/shaders/simple.frag.glsl
+++ GameLibrary/res/shaders/simple.frag.glsl
@@ -1,7 +1,10 @@
 #version 330 core
 
+in vec3 iColor;
+
 uniform vec3 color;
 
 void main() {
-	gl_FragColor = vec4(color, 1.0);
+	vec3 fcol = color * iColor;
+	gl_FragColor = vec4(fcol, 1.0);
 }
--- GameLibrary/src/Main.cpp
+++ GameLibrary/src/Main.cpp
@@ -4,21 +4,26 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 
+using glm::vec2;
+using glm::vec3;
+
 class Application : public Game::App {
 public:
 	virtual void OnCreate() override {
-		glm::vec2 vertices[] = {
-			glm::vec2{ 0.0f, 1.0f},
-			glm::vec2{ 1.0f,-1.0f},
-			glm::vec2{-1.0f,-1.0f},
+		vec3 vertices[] = {
+			vec3{ 0.0f, 0.5f, 0.0f}, vec3{1.0f, 0.0f, 0.0f},
+			vec3{ 0.5f,-0.5f, 0.0f}, vec3{0.0f, 1.0f, 0.0f},
+			vec3{-0.5f,-0.5f, 0.0f}, vec3{0.0f, 0.0f, 1.0f},
 		};
 		glGenVertexArrays(1, &m_VAO);
 		glBindVertexArray(m_VAO);
 		glGenBuffers(1, &m_VBO);
 		glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), static_cast<void*>(0));
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vec3) * 2, (void*)0);
 		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(vec3) * 2, (void*)sizeof(vec3));
+		glEnableVertexAttribArray(1);
 
 		m_Shader = Game::Shader({
 			{ "res/shaders/simple.vert.glsl", Game::Shader::VERTEX_SHADER },
@@ -33,10 +38,10 @@ public:
 	virtual void OnUpdate(double dt) override {
 		m_Accum += dt;
 		if (static_cast<int>(m_Accum) % 2 == 0) {
-			m_Color.r += dt;
+			m_Color.r -= dt;
 		}
 		else {
-			m_Color.r -= dt;
+			m_Color.r += dt;
 		}
 		if (static_cast<int>(m_Accum + 0.5) % 2 == 0) {
 			m_Color.g += dt;
@@ -71,6 +76,6 @@ private:
 
 int main(int argc, char** argv) {
 	Application app;
-	if (app.Create("Application", 800, 450))
+	if (app.Create("Application", 800, 600))
 		app.Run();
 }
